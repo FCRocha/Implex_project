96bb3f138a81bdb8b644e079de2bd605e618e7b6
FCRocha <felipe_rocha@ufms.br>
2023-06-30 10:56:44 -0400

    feat: Novos comentários no Utils e HC

--- Hill_Climbing.py
+++ Hill_Climbing.py
@@ -1,12 +1,12 @@
 import Utils
 import random
-
+#Calcula a distância percorrida no grafo
 def solutionDistance(graph, solution):
     distance = 0
     for i in range(len(solution) - 1):
         distance += graph.distanceBetweenVertices(solution[i], solution[i+1])
     return distance
-
+#Armazena as cidades e um index (vizinhos)
 def getNeighbours(solution):
     neighbours = []
     for i, city in enumerate(solution):
@@ -20,15 +20,18 @@ def getNeighbours(solution):
 def run(path):
     graph = Utils.creatGraph(path)
     cities = graph.getAllVerticesIndex().tolist()
-
+    
+    #Primeira cidade
     solution = random.sample(cities, len(cities))
     distance = solutionDistance(graph, solution)
     neighbours = getNeighbours(solution)
 
     while neighbours:
+        #Calculo a distancia de cada vizinho para encontrar o melhor
         bestNeighbour = min(neighbours, key=lambda x: solutionDistance(graph, x))
         bestNeighbourDistance = solutionDistance(graph, bestNeighbour)
 
+        #Comparo a distancia atual com a do melhor vizinho
         if bestNeighbourDistance < distance:
             solution = bestNeighbour
             distance = bestNeighbourDistance
--- Utils.py
+++ Utils.py
@@ -1,16 +1,16 @@
 import numpy as np
 import math
 
-
+#Tranforma os dados de TXT em um vetor Numpy
 def generateVector(path):
     with open(path) as f_input:
-        text = [" ".join(l.split()) for l in f_input]
+        text = [" ".join(l.split()) for l in f_input]#remove espaços e tem como resultado lista de String
 
     vector = np.loadtxt(text, dtype=float, delimiter=" ").astype(int)
 
     return vector
 
-
+#Cria e retorna um objeto Graph com base no vetor Numpy
 def creatGraph(path):
     vector = generateVector(path)
 
@@ -24,19 +24,21 @@ class Graph:
         self.vector = vector
 
     def distanceBetweenVertices(self, v1, v2):
+        #Encontra as coordenadas  de v1 e v2
         coordsV1 = [self.vector[v1 - 1][1], self.vector[v1 - 1][2]]
         coordsV2 = [self.vector[v2 - 1][1], self.vector[v2 - 1][2]]
+        #Calcula a distancia euclidiana entre v1 e v2
         d = math.sqrt(
             ((coordsV1[0] - coordsV2[0]) ** 2) + ((coordsV1[1] - coordsV2[1]) ** 2)
         )
         return d
-
+    #Gera uma lista com as cooerdenadas dos vértices do grafo
     def getVerticeCoords(self, v):
         result = []
         for x_axis in range(v):
             for y_axis in range(v):
                 result.append((x_axis, y_axis))
         return result
-
+    #Retorna todos os index do grafo
     def getAllVerticesIndex(self):
         return self.vector[:, 0]
